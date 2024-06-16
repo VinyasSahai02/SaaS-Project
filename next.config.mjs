23856489ba7c46d540ca0c5071130b80e251a288
VinyasSahai02 <142114422+VinyasSahai02@users.noreply.github.com>
2024-06-17 01:57:29 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        remotePatterns:[{
+            protocol:"https",
+            hostname:"res.cloudinary.com",
+            port:''
+        }]
+    }
+};
+
+export default nextConfig;
